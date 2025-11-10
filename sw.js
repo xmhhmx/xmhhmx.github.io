651282cd473f18f6e0e71acded691cbb056ca333
mX1@0 <mX1@0@xy.com>
2025-11-10 22:07:06 +0800

    Site updated: 2025-11-10 22:07:03

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1761998862918";const preCache = ["/images/taichi.png","/images/%E7%8E%9B%E8%8E%8E%E6%8B%89%E8%92%82.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1762783531353";const preCache = ["/images/taichi.png","/images/%E7%8E%9B%E8%8E%8E%E6%8B%89%E8%92%82.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
